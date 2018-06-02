e9cd7a507a7ee74c59822b5db7f37007c81c7e7f
dmaflab <danielmaflab@hotmail.com>
2018-06-02 15:59:33 -0500

    crud finalizado

--- src/app/nuevopersonal/nuevopersonal.component.ts
+++ src/app/nuevopersonal/nuevopersonal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { PersonalService } from '../services/personal.service';
 import { Personal } from '../models/personal.model';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-nuevopersonal',
@@ -11,17 +12,36 @@ import { Personal } from '../models/personal.model';
 export class NuevopersonalComponent implements OnInit {
   personal: Personal = new Personal();
   constructor(
-    public _personalService: PersonalService
-  ) { }
+    public _personalService: PersonalService,
+    public activatedRoute: ActivatedRoute,
+    public router: Router
+  ) {
+    activatedRoute.params.subscribe(params => {
+    // tslint:disable-next-line:prefer-const
+    let id = params['id'];
+    if ( id !== 'nuevo') {
+      console.log(id);
+      this.cargarPersonal(id);
+    }
+    });
+  }
 
   ngOnInit() {
   }
+  cargarPersonal(id) {
 
+  this._personalService.cargarUnPersonal( id ).subscribe(personal => this.personal = personal);
+  }
 
   guardarPersonal(f: NgForm) {
 
-    console.log(f.valid);
-    console.log(f.value);
+  if (f.invalid) {
+    return;
+  }
+
+  this._personalService.guardarPersonal(this.personal).subscribe(personal => {
+    console.log(personal);
+  });
 
   }
 }
--- src/app/services/personal.service.ts
+++ src/app/services/personal.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
+import { Personal } from '../models/personal.model';
 
 
 @Injectable()
@@ -22,6 +23,18 @@ cargarPersonal() {
 
 }
 
+cargarUnPersonal(id: string ) {
+
+  console.log(id);
+  // tslint:disable-next-line:prefer-const
+  let url = 'http://localhost:3000/personal/uno/' + id;
+  return this.http.get(url)
+  .map((resp: any) => {
+    console.log(resp.personal, 'trae esta info');
+    return resp.personal;
+  });
+
+}
 
 // ***************************
 /* buscarMedicos( termino: string ) {
@@ -37,6 +50,25 @@ borrarPersonal( id: string) {
   // tslint:disable-next-line:prefer-const
   let url = 'http://localhost:3000/personal/' + id;
   return this.http.delete( url );
+}
+
+// ***************************
+guardarPersonal( personal: Personal) {
+
+  // tslint:disable-next-line:prefer-const
+  let url = 'http://localhost:3000/personal';
+
+  if (personal._id) {
+ url += '/' + personal._id ;
+ return this.http.put(url, personal).map((resp: any) => {
+   return resp.personal;
+ });
+  } else {
+return this.http.post(url, personal).map((resp: any) => {
+  return resp.personal;
+});
+
+}
 }
 
 }
